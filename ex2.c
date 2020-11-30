b38a0e8b6bfd1b98e1a43bc327eb01611802d7ff
Dov Moshe <dovbarak.moshe@live.biu.ac.il>
2020-11-30 20:40:31 +0200

    before the last tests

--- ex2.c
+++ ex2.c
@@ -1,58 +1,58 @@
 #include <stdio.h>
 #include <string.h>
 
-void firstCase(int argc, char const *argv[], FILE* file, FILE* newFile);
+void firstCase(FILE* file, FILE* newFile);
 void secondAndThirdCase(int argc, char const* argv[], FILE* file, FILE* newFile);
 void carriageReturnCase(int argc, char const* argv[], FILE** file, FILE** newFile, int isBig, char* arr);
 void lineFeedCase(int argc, char const* argv[], FILE** file, FILE** newFile, int isBig, char* arr);
 
 /**
-*
-*
-**/
+ * The main function. The main open the source and destination files.
+ * If there is a one argument then jumping to 'return 0'.
+ * Else, if there is 2 argument then go to the func 'firstCase' else go to the func 'secondAndThirdCase'.
+ */
 int main(int argc, char const *argv[]) {
-    // open the read file
-    FILE *file;
-    file = fopen(argv[1], "rb");
-    // open the write file
-    FILE *newFile;
-    newFile = fopen(argv[2], "w");
     // if 'argc' < 3 then there is one argument
-
     if(argc >= 3) {
+        // open the read file
+        FILE *file;
+        file = fopen(argv[1], "rb");
+        if (file == NULL) {
+            return 1;
+        }
+        // open the write file
+        FILE *newFile;
+        newFile = fopen(argv[2], "w");
         if(argc == 3) {
             // this is the simple case
-            firstCase(argc, argv, file, newFile);
+            firstCase(file, newFile);
         } else if (argc == 5 || argc == 6) {
             // checking both the second and third cases
             secondAndThirdCase(argc, argv, file, newFile);
         }
+        fclose(file);
+        fclose(newFile);
     }
-    fclose(file);
-    fclose(newFile);
     return 0;
 }
 
 /**
-*
-*
-**/
-void firstCase(int argc, char const *argv[], FILE* file, FILE* newFile) {
+ * This func just copy the file. reading and writing two bytes by two bytes.
+ */
+void firstCase(FILE* file, FILE* newFile) {
     char arr[2];
     while (fread(&arr, sizeof(arr), 1, file)) {
-        unsigned char* u = (char*)&arr;
-        printf("%x\n", u[0]);
-        printf("%x\n", u[1]);
         fwrite(&arr , sizeof(arr[0]) , sizeof(arr)/sizeof(arr[0]), newFile);
     }
 }
 
-
-
 /**
-*
-*
-**/
+ *
+ * @param argc
+ * @param argv
+ * @param file
+ * @param newFile
+ */
 void secondAndThirdCase(int argc, char const* argv[], FILE* file, FILE* newFile) {
     // to get two byte by two byte from the file
     char arr[2];
@@ -64,19 +64,17 @@ void secondAndThirdCase(int argc, char const* argv[], FILE* file, FILE* newFile)
     if(u[0] == 0xff && u[1] == 0xfe) {
         isBigEndian = 0;
     }
-    // if it is needed to swap the endianness
+    /*// if it is needed to swap the endianness
     if(argc >= 6 && strcmp(argv[5], "-swap") == 0) {
         u[0] = u[0] ^ u[1];
         u[1] = u[0] ^ u[1];
         u[0] = u[0] ^ u[1];
     }
     // write the bytes into the new file
-    fwrite(&arr , sizeof(arr[0]) , sizeof(arr)/sizeof(arr[0]), newFile);
+    fwrite(&arr , sizeof(arr[0]) , sizeof(arr)/sizeof(arr[0]), newFile);*/
 
     // read the file two byte by two byte
-    while (fread(&arr, sizeof(arr), 1, file)) {
-        //printf("%02hhX %02hhX\n", u[1], u[0]);
-
+    do {
         // if the source and dest are equal system then there is no point to check '\r' or '\n'
         if(strcmp(argv[3], argv[4]) !=0) {
             // Because there is no distinction here between the both big and litte,
@@ -87,7 +85,6 @@ void secondAndThirdCase(int argc, char const* argv[], FILE* file, FILE* newFile)
                 carriageReturnCase(argc, argv, &file, &newFile, isBigEndian, arr);
             } else if((isBigEndian == 1 && u[0] == 0x00 && u[1] == 0x0a) ||
             (isBigEndian == 0 && u[0] == 0x0a && u[1] == 0x00)) {
-                //printf("here2\n");
                 // handle with case of '\n'
                 lineFeedCase(argc, argv, &file, &newFile, isBigEndian, arr);
             }
@@ -98,21 +95,30 @@ void secondAndThirdCase(int argc, char const* argv[], FILE* file, FILE* newFile)
             u[1] = u[0] ^ u[1];
             u[0] = u[0] ^ u[1];
         }
-        //printf("%s", arr);
         // write the bytes into the new file
         fwrite(&arr , sizeof(arr[0]) , sizeof(arr)/sizeof(arr[0]), newFile);
-    }
+    } while (fread(&arr, sizeof(arr), 1, file));
 }
 
-
+/**
+ *
+ * @param argc
+ * @param argv
+ * @param file
+ * @param newFile
+ * @param isBig
+ * @param arr
+ */
 void carriageReturnCase(int argc, char const* argv[], FILE** file, FILE** newFile, int isBig, char* arr) {
     char tempArr[2];
     if (strcmp(argv[3], "-win") == 0) {
         if (strcmp(argv[4], "-unix") == 0 && fread(&tempArr , 2, 1, *file)) {
-            printf("here2\n");
             // then jump to next 2 byte
             arr[0] = tempArr[0];
             arr[1] = tempArr[1];
+        } else if(strcmp(argv[4], "-mac") == 0) {
+            // just read '\n' and do nothing
+            fread(&tempArr , 2, 1, *file);
         }
     } else if (strcmp(argv[3], "-mac") == 0) {
         if (strcmp(argv[4], "-unix") == 0) {
@@ -144,16 +150,17 @@ void carriageReturnCase(int argc, char const* argv[], FILE** file, FILE** newFil
     }
 }
 
+/**
+ *
+ * @param argc
+ * @param argv
+ * @param file
+ * @param newFile
+ * @param isBig
+ * @param arr
+ */
 void lineFeedCase(int argc, char const* argv[], FILE** file, FILE** newFile, int isBig, char* arr) {
-    char tempArr[2];
-    if (strcmp(argv[3], "-win") == 0) {
-        if (strcmp(argv[4], "-mac") == 0 && fread(&tempArr, 2, 1, *file)) {
-            // then jump to next 2 byte, because there is on '\n' in mac
-            arr[0] = tempArr[0];
-            arr[1] = tempArr[1];
-        }
-
-    } else if(strcmp(argv[3], "-unix") == 0) {
+    if(strcmp(argv[3], "-unix") == 0) {
         if(strcmp(argv[4], "-mac") == 0) {
             // if the destination is mac then change the '\n' to '\r'
             if (isBig == 1) {
